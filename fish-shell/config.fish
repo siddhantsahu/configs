1624a7d9271e77f3d4026151f772497ba1df64a3
Siddhant Sahu <siddhant.sahu@sokrati.com>
2016-11-12 22:53:51 +0530

    Configs for fish setup
    Prompt for displaying only two upper directories

---
+++ fish-shell/config.fish
@@ -0,0 +1,13 @@
+# autojump configuration
+if test -f /home/siddhant.sahu/.autojump/share/autojump/autojump.fish; . /home/siddhant.sahu/.autojump/share/autojump/autojump.fish; end
+
+# declaring aliases
+alias l="ls -lhtr"
+alias ll="ls -lA"
+alias rm="rm -i"
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias top="htop"
+alias f="find . -name "
+alias jq="python -m json.tool"
